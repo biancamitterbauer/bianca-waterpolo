97b3cc205646e887600660704394cef9e7f4d469
Bianca Mitterbauer <bianca.mitterbauer@t-online.de>
2026-02-18 11:20:27 +0100

    pwa: switch to universal launch overlay and generic startup config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { LayoutComponent } from './core/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent],
-  template: `<app-layout></app-layout>`,
+  imports: [CommonModule, LayoutComponent],
+  templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App {
-  protected readonly title = signal('bianca-waterpolo');
+export class App implements OnInit {
+  launchActive = true;
+  private readonly isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    if (!this.isBrowser) {
+      this.launchActive = false;
+    }
+  }
+
+  ngOnInit(): void {
+    if (!this.isBrowser) {
+      return;
+    }
+
+    setTimeout(() => {
+      this.launchActive = false;
+    }, 520);
+  }
 }
