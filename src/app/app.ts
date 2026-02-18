2935e933cb527ac25db5ea9f741a40c84b45cf12
Bianca Mitterbauer <bianca.mitterbauer@t-online.de>
2026-02-18 13:18:45 +0100

    Remove intro animation completely from app shell

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { LayoutComponent } from './core/layout/layout.component';
-import { AppLaunchService } from './core/app-launch/app-launch.service';
 
 @Component({
   selector: 'app-root',
@@ -10,59 +9,4 @@ import { AppLaunchService } from './core/app-launch/app-launch.service';
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App implements OnInit, OnDestroy {
-  appReady = true;
-  runIntro = false;
-
-  private readyTimer?: ReturnType<typeof setTimeout>;
-  private markTimer?: ReturnType<typeof setTimeout>;
-  private readonly isBrowser: boolean;
-
-  constructor(
-    @Inject(PLATFORM_ID) platformId: object,
-    private readonly launch: AppLaunchService
-  ) {
-    this.isBrowser = isPlatformBrowser(platformId);
-  }
-
-  ngOnInit(): void {
-    if (!this.isBrowser) {
-      this.appReady = true;
-      return;
-    }
-
-    const introDisabled = localStorage.getItem('bm_disable_intro') === '1';
-    if (introDisabled) {
-      this.runIntro = false;
-      this.appReady = true;
-      this.launch.markAnimated();
-      return;
-    }
-
-    this.runIntro = this.launch.shouldAnimate();
-    if (!this.runIntro) {
-      this.appReady = true;
-      return;
-    }
-
-    this.appReady = false;
-
-    this.readyTimer = setTimeout(() => {
-      this.appReady = true;
-    }, 120);
-
-    this.markTimer = setTimeout(() => {
-      this.launch.markAnimated();
-      this.runIntro = false;
-    }, 700);
-  }
-
-  ngOnDestroy(): void {
-    if (this.readyTimer) {
-      clearTimeout(this.readyTimer);
-    }
-    if (this.markTimer) {
-      clearTimeout(this.markTimer);
-    }
-  }
-}
+export class App {}
